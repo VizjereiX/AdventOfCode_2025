d7d6144c0aa72812e077f375c7212d984c203bbb
Marcin Szajek <marcin.szajek@gmail.com>
2025-12-04 08:55:37 +0100

    better version of 3rd day solution

--- tasks/day_03/part_1.py
+++ tasks/day_03/part_1.py
@@ -1,16 +1,33 @@
 logger = None
 
-def run(filename):
+def my_max(v):
+    idx = 0
+    for i in range(1, len(v)):
+        if v[i] > v[idx]:
+            idx = i
+    return idx
+
+def get_batt_joltage(filename, batt_cnt, logger):
     acc = 0
     with open(filename, "r") as f:
         for data in f.read().splitlines():
             v = [int(c) for c in data.strip()]
-            n1 = max(v[0:-1])
-            pos = data.index(str(n1))
-            
-            n2 = max(v[pos+1::])
-            m= n1*10 + n2  
-            logger.info(f"{data}\t{m}")
-            acc+=m
+            reconstructed = []
 
-    return str(acc)
+            while len(reconstructed) < batt_cnt:
+                idx = my_max(v[0: len(v) - batt_cnt + 1 + len(reconstructed)])
+                reconstructed.append(v[idx])
+                if idx < len(v):
+                    v = v[idx+1::]
+                else:
+                    v = []
+                logger.debug(f"{reconstructed}\t{v}")
+
+
+            m = int("".join([str(e) for e in reconstructed]))
+            logger.info(f"{data} -> {m}")
+            acc+=m
+    return acc
+
+def run(filename):
+    return str(get_batt_joltage(filename, 2), logger)
--- tasks/day_03/part_2.py
+++ tasks/day_03/part_2.py
@@ -1,52 +1,5 @@
-logger = None
-
-"168193674087539 <-- too low"
-"168193674087539"
-"169782750711259"
-"169935154100102"
-
-def my_min(v):
-    idx = 0
-    for i in range(1, len(v)):
-        if v[i] < v[idx]:
-            idx = i
-    return idx
-
-def my_max(v):
-    idx = 0
-    for i in range(1, len(v)):
-        if v[i] > v[idx]:
-            idx = i
-    return idx
-
-
-from itertools import combinations
-
-def arr_to_int(v):
-    if not v:
-        return ""
-    return int("".join([str(e) for e in v]))
-
+import tasks.day_03.part_1 as part_1
+global logger
 
 def run(filename):
-    acc = 0
-    with open(filename, "r") as f:
-        for data in f.read().splitlines():
-            v = [int(c) for c in data.strip()]
-            reconstructed = []
-
-            while len(reconstructed) < 12:
-                idx = my_max(v[0: len(v) - 11 + len(reconstructed)])
-                reconstructed.append(v[idx])
-                if idx < len(v):
-                    v = v[idx+1::]
-                else:
-                    v = []
-                logger.debug(f"{reconstructed}\t{v}")
-
-
-            m = int("".join([str(e) for e in reconstructed]))
-            logger.info(f"{data} -> {m}")
-            acc+=m
-
-    return str(acc)
+    return str(part_1.get_batt_joltage(filename, 12, logger))
